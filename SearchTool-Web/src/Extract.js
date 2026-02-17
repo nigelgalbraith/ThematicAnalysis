f38ce0a3aba2c99bc90ecbc114d58a11f4f06f2d
nigelgalbraith <nigelgalbraith@proton.me>
2026-02-18 12:00:27 +1300

    Initial Commit

---
+++ SearchTool-Web/src/Extract.js
@@ -0,0 +1,10 @@
+class Extract {
+  constructor(description, example) {
+    this.description = description;
+  }
+
+  // Extract to string method
+  toString() {
+    return `Extract: ${this.description}`;
+  }
+}
